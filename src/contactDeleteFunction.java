043b7afdf26ecb43478931f488c5e26913cd025e
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-28 15:07:27 -0500

    saving display contacts

--- src/contactDeleteFunction.java
+++ src/contactDeleteFunction.java
@@ -13,17 +13,17 @@ import static java.nio.file.Files.readAllLines;
 public class contactDeleteFunction {
     public static void main(String[] args) throws IOException {
         Input in = new Input();
-        Path filePath = Paths.get("data", "contacts-list.txt");
-
-        try {
-            lines = Files.readAllLines(filePath);
-            String delete = in.getString("Who's contact do you want to delete?");
-            Files.write(filePath, Arrays.asList(delete));
-            Files.deleteIfExists(Path.of("util: contacts-list.txt"));
-            lines.clear(delete);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        Path filePath = Paths.get("data", "contacts-list.txt");
+//
+//        try {
+//            lines = Files.readAllLines(filePath);
+//            String delete = in.getString("Who's contact do you want to delete?");
+//            Files.write(filePath, Arrays.asList(delete));
+//            Files.deleteIfExists(Path.of("util: contacts-list.txt"));
+//            lines.clear(delete);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 
     }
---
+++ src/util/displayContacts.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class displayContacts {
+    public static void main(String[] args) throws IOException {
+        Input in = new Input();
+
+        Path contactsPath = Paths.get("data", "contacts-list.txt");
+        List<String> contactsList = Files.readAllLines(contactsPath);
+
+        for (int i = 0; i < contactsList.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactsList.get(i));
+        }
+    }
+}
